ec67c5a367bed01d0bfaf55438150e6ff5dd1ba8
Alan Russell <ajrussellaudio@gmail.com>
2014-04-30 22:41:14 +0100

    done 08

--- 08_book_titles/book.rb
+++ 08_book_titles/book.rb
@@ -1,2 +1,24 @@
 class Book
+
+	def title()
+		@title
+	end
+
+	def title=(string)
+		@title = make_caps(string)
+	end 
+
+	def make_caps(string)
+		ignore = ["the", "a", "an", "and", "in", "of"]
+		string = string.capitalize
+		array = string.split(" ")
+		i = 0
+		while i < array.length
+			if (not ignore.include?(array[i]))
+				array[i] = array[i].capitalize
+			end
+			i = i + 1
+		end
+		array.join(" ")
+	end
 end
